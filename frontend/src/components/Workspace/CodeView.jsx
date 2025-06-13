f1beb272228fec756d4c32854c2fe389415d60c1
vinoth <mohanrajvinoth2004@gmail.com>
2025-06-14 00:17:53 +0530

    add: chat and code view of the project

--- frontend/src/components/Workspace/CodeView.jsx
+++ frontend/src/components/Workspace/CodeView.jsx
@@ -1,7 +1,69 @@
 import React from "react";
-
+import {
+  SandpackProvider,
+  SandpackLayout,
+  SandpackCodeEditor,
+  SandpackPreview,
+  SandpackFileExplorer,
+} from "@codesandbox/sandpack-react";
+import { useState } from "react";
+import { DEFAULT_FILE, DEPENDENCY } from "../../data/AiDetails";
 const CodeView = () => {
-  return <div>CodeView</div>;
+  const [activeTab, setActiveTab] = useState("code");
+  const [files, setFiles] = useState(DEFAULT_FILE);
+  return (
+    <div>
+      <div className="flex items-center flex-wrap shrink-0 bg-black">
+        <h3
+          className={
+            activeTab === "code" ? "text-shadow-blue-400 bg-amber-100" : ""
+          }
+          onClick={() => setActiveTab("code")}
+        >
+          Code
+        </h3>
+        <h3
+          className={
+            activeTab === "preview" ? "text-shadow-blue-400 bg-amber-100" : ""
+          }
+          onClick={() => setActiveTab("preview")}
+        >
+          Preview
+        </h3>
+      </div>
+      <div>
+        <SandpackProvider
+          template="react"
+          theme={"dark"}
+          files={files}
+          customSetup={{
+            dependencies: {
+              ...DEPENDENCY,
+            },
+          }}
+          options={{
+            externalResources: ["https://cdn.tailwindcss.com",],
+          }}
+        >
+          <SandpackLayout>
+            {activeTab == "code" ? (
+              <>
+                <SandpackFileExplorer style={{ height: "80vh" }} />
+                <SandpackCodeEditor style={{ height: "80vh" }} />
+              </>
+            ) : (
+              <>
+                <SandpackPreview
+                  style={{ height: "80vh" }}
+                  showNavigator={true}
+                />
+              </>
+            )}
+          </SandpackLayout>
+        </SandpackProvider>
+      </div>
+    </div>
+  );
 };
 
 export default CodeView;
