d2fc7b6c54a66941b4165196f84674a7a4a9db08
vinoth <mohanrajvinoth2004@gmail.com>
2025-06-17 19:22:42 +0530

    add price page

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,19 +4,23 @@ import routes from "./routes";
 import { MessageProvider } from "./context/MsgContext";
 import { UserProvider } from "./context/UserContext";
 import { GoogleOAuthProvider } from "@react-oauth/google";
+import { PayPalScriptProvider, PayPalButtons } from "@paypal/react-paypal-js";
 
 function App() {
-  const clientId = import.meta.env.VITE_CLIENT_ID;
+  const googleClientId = import.meta.env.VITE_CLIENT_ID;
+  const payPalClientId = import.meta.env.VITE_PAYPAL_CLIENT_ID;
   const router = createBrowserRouter(routes);
   return (
     <>
-      <GoogleOAuthProvider clientId={clientId}>
-        <UserProvider>
-          <MessageProvider>
-            <RouterProvider router={router} />
-            <ToastContainer />
-          </MessageProvider>
-        </UserProvider>
+      <GoogleOAuthProvider clientId={googleClientId}>
+        <PayPalScriptProvider options={{ clientId: payPalClientId }}>
+          <UserProvider>
+            <MessageProvider>
+              <RouterProvider router={router} />
+              <ToastContainer />
+            </MessageProvider>
+          </UserProvider>
+        </PayPalScriptProvider>
       </GoogleOAuthProvider>
     </>
   );
